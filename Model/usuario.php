cc5b60572de821e40b724ae77b62530c59d3097b
Flávio Jr <joseflavios.jr@gmail.com>
2017-10-07 22:51:01 -0300

    Model Class usuario

---
+++ Model/usuario.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Flávio Jr
+ * Date: 07/10/2017
+ * Time: 22:44
+ */
+
+class usuario {
+
+    public $nome;
+
+    /**
+     * @return mixed
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    
+
+
+}
