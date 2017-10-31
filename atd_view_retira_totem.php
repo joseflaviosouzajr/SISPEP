32da715439d625ce02112d0879cb34683b0c6abe
joseflaviosouzajr <32494072+joseflaviosouzajr@users.noreply.github.com>
2017-10-31 10:28:58 -0200

    Add files via upload

---
+++ atd_view_retira_totem.php
@@ -0,0 +1,12 @@
+<?php
+include_once '../conf/Conexao.php';
+include_once '../model/modelTotem.php';
+include_once '../control/controlTotem.php';
+
+$totem = new controlTotem();
+
+$totem->retirarSenha();
+
+?>
+
+uhih
