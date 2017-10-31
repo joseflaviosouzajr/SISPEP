cc6a56e0ff05d7a59a292e928ce00718151251b0
José Flávio <joseflavios.jr@gmail.com>
2017-10-31 11:24:17 -0200

    app base

--- app/view/atd_viewRetiraTotem.php
+++ app/view/atd_viewRetiraTotem.php
@@ -1,25 +1,27 @@
-<?php
-include_once '../conf/Conexao.php';
-include_once '../model/modelTotem.php';
-include_once '../control/controlTotem.php';
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../lib/bootstrap/dist/css/bootstrap.min.css"/>
+    <title>SISPEP | Totem Senha</title>
+</head>
+<body>
 
-$cdPrioridadeTotem = 1;
+<div class="jumbotron">
+    <div class="row">
+        <div class="col-md-6 col-xs-12">
+            <h1>Prioridade</h1>
+            <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+        </div>
+        <div class="col-md-6 col-xs-12">
+            <h1>Normal</h1>
+            <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+        </div>
+    </div>
+</div>
 
-$totem = new controlTotem();
-$totem->setCdPrioridadeTotem($cdPrioridadeTotem);
-$cdTotem = $totem->retirarSenha();
-
-if ($cdTotem > 0){
-    echo '
-    <script type="text/javascript">
-        alert("Totem retirado:\n '.$cdTotem.'");
-    </script>
-    ';
-}else{
-    switch (gettype($cdTotem)){
-        case 'string':
-            echo $cdTotem;
-            break;
-    }
-}
-?>
+</body>
+</html>
